81bd9fd114f9b9bf4c155e526fc82137f349043f
Gosu <cgdc.bom@gmail.com>
2018-10-28 12:17:36 +0700

    g

--- SuperMart/src/com/supermart/controllers/admin/LayoutsController.java
+++ SuperMart/src/com/supermart/controllers/admin/LayoutsController.java
@@ -2,17 +2,24 @@ package com.supermart.controllers.admin;
 
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/admin")
 public class LayoutsController {
-  @RequestMapping(method=RequestMethod.GET)
-  public String index(HttpSession session) {
-	  return session.getAttribute("username") == null ? "login" : "admin";
+	@RequestMapping(method=RequestMethod.GET)
+  public String index(HttpSession session, Model model) {
+	  if (session.getAttribute("username") == null) {
+		  return "admin";
+	  } else {
+		  model.addAttribute("username", session.getAttribute("username"));
+		  return "admin";
+	  }
   }
-  @RequestMapping(value = "/hello",method=RequestMethod.GET)
-  public String hello() {
-    return "hello";
-  }
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return "hello";
+	}
 }
